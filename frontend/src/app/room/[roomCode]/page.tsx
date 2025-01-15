6a3f71d4b6e8aa5a08d6730ed412026fdffb4de2
Sam <damakhoucem90@gmail.com>
2025-01-15 16:02:41 +0100

    unused-var-fix

--- frontend/src/app/room/[roomCode]/page.tsx
+++ frontend/src/app/room/[roomCode]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
 import { io, Socket } from "socket.io-client";
 
 interface Player {
